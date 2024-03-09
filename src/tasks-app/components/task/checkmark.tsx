46f968a5e0ab38ae2b0bffd50914e9762037687f
riccardobattiato <rbattiato@outlook.it>
2024-03-09 13:12:00 +0100

    feat(ui): checkmark component

---
+++ src/tasks-app/components/task/checkmark.tsx
@@ -0,0 +1,48 @@
+import { CheckIcon } from "@heroicons/react/16/solid";
+import { clsx } from "clsx";
+import { ChangeEventHandler, useCallback } from "react";
+
+export type TaskCheckmarkProps = {
+  active?: boolean;
+  onChange?: (checked: boolean) => void;
+};
+
+const TaskCheckmark = ({ active, onChange }: TaskCheckmarkProps) => {
+  const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    (e) => {
+      onChange?.(e.target.checked);
+    },
+    [onChange]
+  );
+
+  return (
+    <div className="task-checkmark">
+      <label
+        className={clsx([
+          "task-checkmark__wrapper group relative cursor-pointer inline-block border-stone-400 border rounded-full w-5 h-5",
+          {
+            "text-neutral-800 bg-stone-400": active,
+          },
+        ])}
+      >
+        <input
+          className="task-checkmark__input absolute invisible"
+          type="checkbox"
+          checked={active}
+          onChange={handleChange}
+        />
+        <CheckIcon
+          className={clsx([
+            "w-5/6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300",
+            {
+              "invisible opacity-0 group-hover:visible group-hover:opacity-100":
+                !active,
+            },
+          ])}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default TaskCheckmark;
